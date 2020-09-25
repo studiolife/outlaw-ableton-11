34aeebfd4d27099e3c07aa2d38a8db46f6b7eafa
concept10 <tenthconcept@gmail.com>
2020-09-25 02:36:04 +0000

    Initial code configuring the winston logger.

--- index.js
+++ index.js
@@ -6,3 +6,15 @@
 
 import * as rxjs from "rxjs";
 
+const winston = require('winston');
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') 
